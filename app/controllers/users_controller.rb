c0abc633ec0b5821513b0386ea27a0af7edc75c0
nguyen duy son <nguyenduyson60@gmail.com>
2021-02-25 17:03:05 +0700

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "message_register_success"
       redirect_to new_user_path
     else
